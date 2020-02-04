6b3932bf7651621e80640da77020cec74f1a121b
Casey Lewiston <caseylewiston@gmail.com>
2020-02-03 18:48:19 -0800

    nt

--- site/src/People.js
+++ site/src/People.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { config } from "./phoenix.js";
+import { config } from "./phoenix.config";
 
 class People extends React.Component{
     constructor() {
